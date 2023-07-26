c2283dda20cef56ff28004de5766211828f13c39
RushabhNagpure <114604410+RushabhNagpure@users.noreply.github.com>
2023-07-26 09:51:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArticle(touristSpots) {
+	return touristSpots.replace(/^(a | an | the )/i,'').trim();
+}
+let sortNames = touristSpots.map(removeArticle).sort((a,b) => a.localeCompare(b));
+const ulElement = document.getElementById('band');
+
+sortNames.forEach(touristSpots => {
+	const liElement = document.createElement('li');
+	liElement.textContent = touristSpots;
+	ulElement.appendChild(liElement)
+});
